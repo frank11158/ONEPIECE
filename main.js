b4ede48d5b36a1e95a4db106547b906d0eacbe6f
Fu-Chia Chang <obafgkm11158@gmail.com>
2019-01-08 21:14:04 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -702,6 +702,11 @@ function getCookie(cname) {
     }
     return "";
 }
+
+$('.logout').click(function () {
+    $.cookie('Username', null, -1);
+});
+
 /*------------------------------*/
 function pop(){
     var popmes = document.querySelector(".floating.message.hidden");
@@ -710,5 +715,16 @@ function pop(){
     else{
         popmes = document.querySelector(".floating.message");
         popmes.classList.add("hidden");
+    }
+}
+
+function deleteAllCookies() {
+    var cookies = document.cookie.split(";");
+
+    for (var i = 0; i < cookies.length; i++) {
+        var cookie = cookies[i];
+        var eqPos = cookie.indexOf("=");
+        var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
     }
 }
